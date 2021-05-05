657af9683e81d3bfc2fda2375c31981270a1d4b8
Jeremy Costanza <costanza.jeremy@gmail.com>
2021-05-05 16:00:12 -0400

    build : create mvn repository if not exist

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -e
 git submodule update --init
 
+mkdir -p ~/.m2
+
 docker run --rm --name kf-zeppelin-web-build \
  --user $(id -u):$(id -g) \
  -v "$(pwd)":/usr/src/app \
@@ -17,4 +19,5 @@ docker run --rm --name kf-zeppelin-web-build \
  -w /usr/src/mymaven \
  maven:3.8.1-jdk-11 \
  mvn -Duser.home=/var/maven package
+
 aws s3 cp ./target/zeppelin-web-0.9.1-SNAPSHOT.war s3://kf-strides-variant-parquet-prd/ami_libraries/zeppelin/zeppelin-web-0.9.1-SNAPSHOT.war
