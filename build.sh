2d413a14815a1f9151968877e98e0d48b44eea16
Jeremy Costanza <costanza.jeremy@gmail.com>
2021-05-05 16:09:12 -0400

    build : not mount m2 files

--- build.sh
+++ build.sh
@@ -14,10 +14,8 @@ docker run --rm --name kf-zeppelin-web-build \
 docker run --rm --name kf-zeppelin-web-build \
  --user $(id -u):$(id -g) \
  -v "$(pwd)":/usr/src/mymaven \
- -v ~/.m2:/var/maven/.m2 \
- -e MAVEN_CONFIG=/var/maven/.m2 \
  -w /usr/src/mymaven \
  maven:3.8.1-jdk-11 \
- mvn -Duser.home=/var/maven package
+ mvn package
 
 aws s3 cp ./target/zeppelin-web-0.9.1-SNAPSHOT.war s3://kf-strides-variant-parquet-prd/ami_libraries/zeppelin/zeppelin-web-0.9.1-SNAPSHOT.war
