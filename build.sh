095ff7fff5e3f0cd7626b1bc13ac2358785e847e
Jeremy Costanza <costanza.jeremy@gmail.com>
2021-05-05 13:37:11 -0400

    build : build with 2 dockers (node and mvn)

--- build.sh
+++ build.sh
@@ -2,10 +2,19 @@
 set -e
 git submodule update --init
 
-#docker run --rm --name kf-zeppelein-web-build \
-# --user $(id -u):$(id -g) \
-# -v "$(pwd)":/usr/src/mymaven \
-# -w /usr/src/mymaven maven:3.8.1-jdk-11 \
-# mvn package
-mvn package
+docker run --rm --name kf-zeppelin-web-build \
+ --user $(id -u):$(id -g) \
+ -v "$(pwd)":/usr/src/app \
+ -w /usr/src/app \
+ node:14 \
+ npm run build:dist
+
+docker run --rm --name kf-zeppelin-web-build \
+ --user $(id -u):$(id -g) \
+ -v "$(pwd)":/usr/src/mymaven \
+ -v ~/.m2:/var/maven/.m2 \
+ -e MAVEN_CONFIG=/var/maven/.m2 \
+ -w /usr/src/mymaven \
+ maven:3.8.1-jdk-11 \
+ mvn -Duser.home=/var/maven package
 aws s3 cp ./target/zeppelin-web-0.9.1-SNAPSHOT.war s3://kf-strides-variant-parquet-prd/ami_libraries/zeppelin/zeppelin-web-0.9.1-SNAPSHOT.war
